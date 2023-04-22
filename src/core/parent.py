610545c3a612da2c8c7799feaa045f7134021859
Siddharth <st22041@ormiston.school.nz>
2023-04-22 23:54:55 +1200

    Now adds B/S classifications to the correct column

--- src/core/parent.py
+++ src/core/parent.py
@@ -12,15 +12,33 @@ class parent:
             self.currentLiabilties = self.json.load(file)
         with open("src/data/equity.json", "r") as file:
             self.equity = self.json.load(file)
+    
+    def dump_data(self):
+        for object, path in [
+            (self.currentAssets, "src/data/currentAssets.json"),
+            (self.nonCurrentAssets, "src/data/nonCurrentAssets.json"),
+            (self.currentLiabilties, "src/data/currentLiabilities.json"),
+            (self.nonCurrentLiablities, "src/data/nonCurrentLiabilities.json"),
+            (self.equity, "src/data/equity.json"),
+        ]:
+            with open(path, "w") as file:
+                self.json.dump(object, file)
 
     def check_columns(self):
-        for i in [self.currentAssets, self.nonCurrentAssets, self.currentLiabilties, self.nonCurrentLiablities, self.equity]:
+        for data in [self.currentAssets, self.nonCurrentAssets, self.currentLiabilties, self.nonCurrentLiablities, self.equity]:
             columns = 0
-            for j in i:
-                if isinstance(i[j][0], str):
+            for items in data:
+                if isinstance(data[items][0], str):
                     columns += 1
+            
+            if columns <= 1:
+                for items in data:
+                    data[items][1] = data[items][0]
+                    data[items][0] = 0
             print(columns)
+            print(data)
 
 e = parent()
 e.get_data()
 e.check_columns()
+e.dump_data()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from core.readTrialBalance import readTrialBalance
 
-trial_balance = readTrialBalance("testTB2.png")
+trial_balance = readTrialBalance("testTB3.png")
 trial_balance.get_pytesseract_version()
 trial_balance.get_classifications()
 trial_balance.check_errors()
